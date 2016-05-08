0a9520dbceda93f6a23d9dbf32f2b2cf233fe65f
Arlen Neylon <arlen.m.neylon@gmail.com>
2016-05-07 18:24:29 -0700

    mock html and js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 console.log("test");
+
+function generateText(){
+  console.log("text gen");
+  console.log(words);
+}
---
+++ js/words.js
@@ -0,0 +1,9 @@
+var words = [
+  "diversify",
+  "funds",
+  "stocks",
+  "bonds",
+  "401k",
+  "rollover",
+  "IRA"
+]
