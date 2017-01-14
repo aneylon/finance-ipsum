43d68c3c365a51757f279519e0fc1c567ad531ca
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-01-14 14:10:32 -0800

    begin server branch

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # finance-ipsum
-Generate filler text for your fintech web app mockup.
+Generate filler text for your fin-tech web app mockup.
 ---
 visit the live website [Finance Ipsum](http://www.arlen-neylon.com/finance-ipsum)
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const makeIpsum = require('./makeIpsum')
+
+const port =  process.env.PORT || 8080
+
+app.use(morgan('dev'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(express.static('public'))
+
+app.get('/ipsum/:num', (req, res) => {
+  num = Number(req.params.num)
+  console.log(makeIpsum(num))
+  res.send(makeIpsum(num))
+})
+
+app.get('*', function(req, res){
+  res.redirect('/')
+})
+
+app.listen(port, () => {
+  console.log('listening on', port)
+})
