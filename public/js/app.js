1e047dea0ea49b38b3431493ca46421b8d06d20b
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-01-14 22:32:09 -0800

    move logic to server

--- public/js/app.js
+++ public/js/app.js
@@ -1,85 +1,26 @@
-// make bg image
-// submit with 'enter' in text field.
-
-// input validation
-  // do not allow non number characters in text field
-
-// put in commas for longer sentences
-// initial tab at beginning of paragraph
-// better sentence construction
-
-// google adsense ?
-// url = ~ $15.00
-
 function keyDown(event){
   if( event.which === 13 ) {
-    generateText();
+    generateText()
   }
 }
 
 function generateText(){
-  var numParagraphs = document.getElementById("numParagraphs").value;
-  numParagraphs = Number(numParagraphs);
+  var numParagraphs = document.getElementById("numParagraphs").value
+  numParagraphs = Number(numParagraphs)
 
   if(isNaN(numParagraphs) || numParagraphs === 0)
-    numParagraphs = 1;
-  
+    numParagraphs = 1
+
   if(numParagraphs > 10)
     numParagraphs = 10
 
-  var newText = makeParagraphs(numParagraphs);
-  document.getElementById("finText").innerHTML = newText;
-  document.getElementById("numParagraphs").value = "";
-}
-
-function randNum(min, max){
-  if(max === undefined){
-    var rand = Math.floor(Math.random()*min);
-    return rand < 1 ? 1: rand;
-  }
-    return rand = Math.floor(Math.random()*(max - min) + min);
-}
-
-function makeSentence(words,numWords){
-  var sentenceLength = randNum(3,numWords);
-  var tempWords = words.slice();
-  var sentence = "";
-  for( var i = 0; i < sentenceLength; i++){
-    var rand = randNum(tempWords.length);
-    if(i === 0) {
-      var word = tempWords[rand];
-      var tmp = "";
-      for(var k = 0; k < word.length; k++){
-        if(k === 0) {
-          tmp += word[k].toUpperCase();
-        } else {
-          tmp += word[k];         
-        }
-      }
-      sentence += " " + tmp;
-    } else {
-      sentence += " " + tempWords[rand];
+  var xhttp = new XMLHttpRequest()
+  xhttp.onreadystatechange = function() {
+    if(this.readyState === 4 && this.status === 200) {
+      document.getElementById("finText").innerHTML = this.responseText
+      document.getElementById("numParagraphs").value = ""
     }
-    tempWords.splice(rand,1);
   }
-  return sentence + ".";
-}
-
-function makeParagraph(numSen){
-  var output = "";
-  var numWords = randNum(numSen);
-  for(var i = 0; i < numSen; i++){
-    output += makeSentence(finWords,15);
-  }
-  return output;
-}
-
-function makeParagraphs(number){
-  var output = "";
-  for(var i = 0; i < number; i ++){
-    output += "<p>\n";
-    output += makeParagraph(randNum(2,8)) + "\n";
-    output += "</p>\n";
-  }
-  return output;
+  xhttp.open('GET', '/ipsum/' + numParagraphs, true)
+  xhttp.send()
 }
